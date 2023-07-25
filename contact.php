60d2eebecd1599e27d2584853748c2c1744b894e
Aurits <87270662+Aurits@users.noreply.github.com>
2023-07-25 17:30:05 +0300

    edit

--- contact.php
+++ contact.php
@@ -54,12 +54,9 @@
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg">
                         <a class="navbar-brand" href="index.html">
-                            <img class="rounded" src="assets/images/logo/logo.png?h=cf2dea01fb0b7e2900b4546e2147935f"
-                                width="120vw" height="55vh">
+                            <img class="rounded" src="assets/images/logo/logo.png?h=cf2dea01fb0b7e2900b4546e2147935f" width="120vw" height="55vh">
                         </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="toggler-icon"></span>
                             <span class="toggler-icon"></span>
                             <span class="toggler-icon"></span>
@@ -77,10 +74,7 @@
                                     <a class="page-scroll " href="service-1.html">Service</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="page-scroll dd-menu collapsed" href="javascript:void(0)"
-                                        data-bs-toggle="collapse" data-bs-target="#submenu-1-3"
-                                        aria-controls="navbarSupportedContent" aria-expanded="false"
-                                        aria-label="Toggle navigation">Pages</a>
+                                    <a class="page-scroll dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#submenu-1-3" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Pages</a>
 
                                     <ul class="sub-menu collapse" id="submenu-1-3">
                                         <li class="nav-item"><a href="about.html">About Us</a></li>
@@ -107,8 +101,7 @@
     <!-- ========================= header end ========================= -->
 
     <!-- ========================= page-banner-section start ========================= -->
-    <section class="page-banner-section pt-75 pb-75 img-bg"
-        style="background-image: url('assets/images/bg/common-bg.svg')">
+    <section class="page-banner-section pt-75 pb-75 img-bg" style="background-image: url('assets/images/bg/common-bg.svg')">
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
@@ -117,8 +110,7 @@
                         <div class="page-breadcrumb">
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item" aria-current="page"><a
-                                            href="javascript:void(0)">Home</a></li>
+                                    <li class="breadcrumb-item" aria-current="page"><a href="javascript:void(0)">Home</a></li>
                                     <li class="breadcrumb-item active" aria-current="page">Contact</li>
                                 </ol>
                             </nav>
@@ -183,12 +175,11 @@
                                 <div class="section-title text-center mb-50">
                                     <span class="wow fadeInDown" data-wow-delay=".2s">Get in Touch</span>
                                     <h2 class="wow fadeInUp" data-wow-delay=".4s">Ready to Get Started</h2>
-                                    <p class="wow fadeInUp" data-wow-delay=".6s">Our support team is dedicated to help
-                                        you, just contact us now</p>
+                                    <p class="wow fadeInUp" data-wow-delay=".6s">Our support team is dedicated to help you, just contact us now</p>
                                 </div>
                             </div>
                         </div>
-                        <form action="assets/php/mail.php" class="contact-form">
+                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="contact-form">
                             <div class="row">
                                 <div class="col-md-6">
                                     <input type="text" name="name" id="name" placeholder="Name" required>
@@ -202,25 +193,68 @@
                                     <input type="text" name="phone" id="phone" placeholder="Phone" required>
                                 </div>
                                 <div class="col-md-6">
-                                    <input type="text" name="subject" id="email" placeholder="Subject" required>
+                                    <input type="text" name="subject" id="subject" placeholder="Subject" required>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-12">
-                                    <textarea name="message" id="message" placeholder="Type Message"
-                                        rows="5"></textarea>
+                                    <textarea name="message" id="message" placeholder="Type Message" rows="5" required></textarea>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-12">
                                     <div class="button text-center">
-                                        <button type="submit" class="theme-btn">Send Message</button>
+                                        <button type="submit" class="theme-btn" name="submit">Send Message</button>
                                     </div>
                                 </div>
                             </div>
                         </form>
                     </div>
                 </div>
+
+                <!-- Modified PHP code -->
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+                    // Database connection parameters
+                    $db_host = "localhost";
+                    $db_user = "root";
+                    $db_pass = "";
+                    $db_name = "contact";
+
+                    // Connect to the database
+                    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+                    // Check if the database connection is successful
+                    if (!$conn) {
+                        die("Database connection failed: " . mysqli_connect_error());
+                    }
+
+                    $name = mysqli_real_escape_string($conn, $_POST["name"]);
+                    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+                    $phone = mysqli_real_escape_string($conn, $_POST["phone"]);
+                    $subject = mysqli_real_escape_string($conn, $_POST["subject"]);
+                    $message = mysqli_real_escape_string($conn, $_POST["message"]);
+
+                    // Form validation - Ensure all required fields are filled
+                    if (empty($name) || empty($email) || empty($phone) || empty($subject) || empty($message)) {
+                        echo "<p>Please fill in all the fields.</p>";
+                    } else {
+                        // SQL query to insert data into the "contact" table
+                        $sql = "INSERT INTO contact (name, email, phone, subject, message) VALUES ('$name', '$email', '$phone', '$subject', '$message')";
+
+                        // Execute the query and check if the insertion was successful
+                        if (mysqli_query($conn, $sql)) {
+                            echo "<p>Thank you for your message! We will get back to you soon.</p>";
+                        } else {
+                            echo "<p>Oops! An error occurred while sending your message. Please try again later.</p>";
+                        }
+
+                        // Close the database connection
+                        mysqli_close($conn);
+                    }
+                }
+                ?>
+
             </div>
         </div>
     </section>
@@ -230,10 +264,7 @@
     <section class="map-section">
         <div class="map-container">
             <div class="mapouter">
-                <div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no"
-                        marginheight="0" marginwidth="0"
-                        src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Kampala&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a
-                        href="https://connectionsgame.org/">Connections Game</a></div>
+                <div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Kampala&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://connectionsgame.org/">Connections Game</a></div>
                 <style>
                     .mapouter {
                         position: relative;
@@ -258,20 +289,16 @@
         </div>
     </section>
     <!-- ========================= map-section end ========================= -->
-    <section id="contact" class="contact-section cta-bg img-bg pt-110 pb-100"
-        style="background-image: url('assets/images/bg/cta-bg.jpg');">
+    <section id="contact" class="contact-section cta-bg img-bg pt-110 pb-100" style="background-image: url('assets/images/bg/cta-bg.jpg');">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-xl-5 col-lg-7">
                     <div class="section-title mb-60">
-                        <span class="text-white wow fadeInDown" data-wow-delay=".2s"
-                            style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInDown;">Custom
+                        <span class="text-white wow fadeInDown" data-wow-delay=".2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInDown;">Custom
                             Software</span>
-                        <h2 class="text-white wow fadeInUp" data-wow-delay=".4s"
-                            style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">We Build
+                        <h2 class="text-white wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">We Build
                             Custom Software Tailored to Your Business Needs</h2>
-                        <p class="text-white wow fadeInUp" data-wow-delay=".6s"
-                            style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">Transform your
+                        <p class="text-white wow fadeInUp" data-wow-delay=".6s" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">Transform your
                             business with our custom software solutions. We understand that every business is unique,
                             which is why we develop software specifically tailored to meet your specific requirements.
                             Our team of experts will work closely with you to analyze your needs and deliver a powerful
@@ -292,19 +319,19 @@
             <div class="client-logo-wrapper">
                 <div class="client-logo-carousel d-flex align-items-center justify-content-between">
                     <div class="client-logo">
-                        <img src="assets/images/client-logo/uideck-logo.svg" alt="">
+                        <img src="assets/images/affiliate/azure.png" alt="">
                     </div>
                     <div class="client-logo">
-                        <img src="assets/images/client-logo/pagebulb-logo.svg" alt="">
+                        <img src="assets/images/affiliate/hostinger.png" alt="">
                     </div>
                     <div class="client-logo">
-                        <img src="assets/images/client-logo/lineicons-logo.svg" alt="">
+                        <img src="assets/images/affiliate/github.png" alt="">
                     </div>
                     <div class="client-logo">
-                        <img src="assets/images/client-logo/graygrids-logo.svg" alt="">
+                        <img src="assets/images/affiliate/namecheap.png" alt="">
                     </div>
                     <div class="client-logo">
-                        <img src="assets/images/client-logo/lineicons-logo.svg" alt="">
+                        <img src="assets/images/affiliate/vscode.png" alt="">
                     </div>
                 </div>
             </div>
@@ -312,9 +339,9 @@
     </section>
     <!-- ========================= client-logo-section end ========================= -->
 
+
     <!-- ========================= subscribe-section start ========================= -->
-    <section class="subscribe-section pt-70 pb-70 img-bg"
-        style="background-image: url('assets/images/bg/common-bg.svg')">
+    <section class="subscribe-section pt-70 pb-70 img-bg" style="background-image: url('assets/images/bg/common-bg.svg')">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-xl-6 col-lg-6">
@@ -416,8 +443,7 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <p class="wow fadeInUp" data-wow-delay=".3s"> 2023 &copy <a href="https://lytelink.tech"
-                                rel="nofollow">LyteLink</a></p>
+                        <p class="wow fadeInUp" data-wow-delay=".3s"> 2023 &copy <a href="https://lytelink.tech" rel="nofollow">LyteLink</a></p>
                     </div>
                 </div>
             </div>
